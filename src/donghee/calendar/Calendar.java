6bc07b006ff24f6f23bfc1711e9ca3e04d54feb7
DongheeL <69754661+DongheeL@users.noreply.github.com>
2020-09-02 08:53:02 +0900

    Update calendar function

--- src/donghee/calendar/Calendar.java
+++ src/donghee/calendar/Calendar.java
@@ -1,24 +1,26 @@
 package donghee.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
-		String[] day = {"일","월","화","수","목","금","토"};
-		for(int i=0;i<7;i++) {
-			System.out.print("   "+day[i]);
-		}
-		System.out.println();
-		System.out.println("---------------------------------");
-		for(int j=0;j<4;j++) {
-			int x = 7*j;
-			for(int i=1;i<=7;i++) {
-				if(x+i<10)
-					System.out.print("  "+(x+i));
-				else
-					System.out.print(" "+(x+i));
+		Scanner sc = new Scanner(System.in);
+		System.out.println("달을 입력하세요");
+		System.out.print("MONTH> ");
+		int month = sc.nextInt();
+		final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
+		System.out.printf("%d월은 %d일까지 있습니다.%n",month,MAX_DAYS[month-1]);
+		
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
+		for(int i=1;i<=28;i++) {
+			System.out.printf("%3d",i);
+			if(i%7==0) {
+				System.out.println();
 			}
-			System.out.println();
 		}
+		
 	}
 
 }
